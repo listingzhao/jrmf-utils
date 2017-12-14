5cb00e8e6425f50d69d03d6234c3a362cfd44942
Listing <listing.zhao@gmail.com>
2017-12-14 18:01:57 +0800

    :white_check_mark: add test

---
+++ src/components/array/test.js
@@ -0,0 +1,14 @@
+import { assert } from 'chai'
+import isEqual from './isEqual.js'
+
+describe('mutils API Test', function () {
+  it('should start empty', function () {
+    var arr = []
+
+    assert.equal(arr.length, 0)
+  })
+
+  it(`isEqual([0, 2, 3], [1, 2, 3]) should return false`, () => {
+    assert.notEqual(isEqual([0, 2, 3], [1, 2, 3]))
+  })
+})
