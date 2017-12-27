0ae0055cca3a6ce0635886fcdc15eb10de938211
Listing <listing.zhao@gmail.com>
2017-12-27 09:49:48 +0800

    :white_check_mark: fix scroll test

--- src/components/scroll/index.js
+++ src/components/scroll/index.js
@@ -2,7 +2,7 @@
  * [getScrollTop]
  * @return {[type]} [description]
  */
-function getScrollTop () {
+export function getScrollTop () {
   const supportPageYOffset = window.pageYOffset !== undefined
   const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat')
   return supportPageYOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
@@ -12,7 +12,7 @@ function getScrollTop () {
  * [setScrollTop description]
  * @param {[type]} value [description]
  */
-function setScrollTop (value) {
+export function setScrollTop (value) {
   window.scrollTo(0, value)
 }
 
