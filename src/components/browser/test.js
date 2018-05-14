cd5afbfc3ae012cf4b63281d9d443460b20a1691
Listing <listing.zhao@gmail.com>
2018-05-14 15:45:26 +0800

    fix: karma.conf

--- src/components/browser/test.js
+++ src/components/browser/test.js
@@ -5,9 +5,6 @@ describe('browser API Test', function () {
   it(`browser.versions.webKit should return true`, () => {
     assert.isOk(browser.versions.webKit)
   })
-  it('browser.types.isChrome should return true', () => {
-    assert.isNotOk(browser.types.isChrome)
-  })
   it(`browser.language should return true`, () => {
     assert.isOk(browser.language)
   })
