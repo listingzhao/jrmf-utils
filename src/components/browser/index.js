a2a57cae44ad546df7a1d3a070afa566f29ed810
Listing <listing.zhao@gmail.com>
2018-05-14 13:21:27 +0800

    feat: browser.types

--- src/components/browser/index.js
+++ src/components/browser/index.js
@@ -7,7 +7,16 @@ const browser = {
       android: ua.indexOf('Android') > -1 || ua.indexOf('Linux') > -1,
       ios: /\(i[^;]+;( U;)? CPU.+Mac OS X/.test(ua)
     }
-  }()),
+  })(),
+  types: (function () {
+    const ua = navigator.userAgent
+    return {
+      isChrome: ua.indexOf('Chrome') && window.chrome,
+      isIE: window.ActiveXObject !== undefined && ua.indexOf('MSIE') !== -1,
+      isFireFox: ua.indexOf('FireFox') !== -1,
+      isSafari: ua.indexOf('Safari') !== -1 && ua.indexOf('Version') !== -1
+    }
+  })(),
   language: (navigator.browserLanguage || navigator.language).toLowerCase()
 }
 
--- src/components/browser/test.js
+++ src/components/browser/test.js
@@ -5,6 +5,9 @@ describe('browser API Test', function () {
   it(`browser.versions.webKit should return true`, () => {
     assert.isOk(browser.versions.webKit)
   })
+  it('browser.types.isChrome should return true', () => {
+    assert.isNotOk(browser.types.isChrome)
+  })
   it(`browser.language should return true`, () => {
     assert.isOk(browser.language)
   })
