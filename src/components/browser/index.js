255b88a32be238f3be81639fb9bb0a495487768a
Listing <listing.zhao@gmail.com>
2018-05-15 14:09:52 +0800

    feat: browser

--- src/components/browser/index.js
+++ src/components/browser/index.js
@@ -1,6 +1,6 @@
+const ua = navigator.userAgent
 const browser = {
   versions: (function () {
-    const ua = navigator.userAgent
     return {
       webKit: ua.indexOf('AppleWebKit') > -1,
       gecko: ua.indexOf('Gecko') !== -1,
@@ -10,7 +10,6 @@ const browser = {
     }
   })(),
   types: (function () {
-    const ua = navigator.userAgent
     return {
       isChrome: ua.indexOf('Chrome') && window.chrome,
       isIE: window.ActiveXObject !== undefined && ua.indexOf('MSIE') !== -1,
@@ -18,6 +17,74 @@ const browser = {
       isSafari: ua.indexOf('Safari') !== -1 && ua.indexOf('Version') !== -1
     }
   })(),
+  ctype: function () {
+    let _self = this
+    if (_self.versions.webKit || _self.versions.gecko) {
+      return 'pc'
+    } else if (
+      _self.versions.android ||
+      _self.versions.ios ||
+      _self.versions.wechat
+    ) {
+      return 'h5'
+    }
+  },
+  brower: function () {
+    let _self = this
+    if (_self.ctype() === 'pc') {
+      if (_self.types.isChrome) {
+        return 'chrome'
+      } else if (navigator.userAgent.indexOf('Firefox') !== -1) {
+        return 'firefox'
+      } else if (_self.types.isIE) {
+        return 'ie'
+      } else if (_self.types.isSafari) {
+        return 'safari'
+      }
+    }
+  },
+  browerVer: function () {
+    let btype = this.brower()
+    let version
+    if (btype) {
+      var name = btype.substring(0, 1).toUpperCase() + btype.substring(1)
+      version = ua.substring(ua.indexOf(name))
+      if (version.indexOf(' ') !== -1) {
+        version = version.substring(
+          version.indexOf('/') + 1,
+          version.indexOf(' ')
+        )
+      } else {
+        version = version.substring(version.indexOf('/') + 1)
+      }
+      return version
+    }
+    return ''
+  },
+  osSys: function () {
+    let isWin = navigator.platform === 'Win32' || navigator.platform === 'Windows'
+    let isMac =
+      navigator.platform === 'MacIntel' ||
+      navigator.platform === 'Mac68K' ||
+      navigator.platform === 'MacPPC' ||
+      navigator.platform === 'Macintosh'
+    if (isMac) return 'mac'
+    if (isWin) {
+      let isWinXP =
+        ua.indexOf('Windows NT 5.1') > -1 || ua.indexOf('Windows XP') > -1
+      if (isWinXP) return 'winxp'
+      let isWin7 =
+        ua.indexOf('Windows NT 6.1') > -1 || ua.indexOf('Windows 7') > -1
+      if (isWin7) return 'win7'
+      let isWin10 =
+        ua.indexOf('Windows NT 10') > -1 || ua.indexOf('Windows 10') > -1
+      if (isWin10) return 'win10'
+    }
+  },
+  osVersion: function () {
+    let osVer = ua.substring(ua.indexOf('(') + 1, ua.indexOf(')'))
+    return osVer
+  },
   language: (navigator.browserLanguage || navigator.language).toLowerCase()
 }
 
--- src/components/browser/test.js
+++ src/components/browser/test.js
@@ -5,6 +5,27 @@ describe('browser API Test', function () {
   it(`browser.versions.webKit should return true`, () => {
     assert.isOk(browser.versions.webKit)
   })
+  it(`browser.versions.ctype should return pc`, () => {
+    console.log(browser.ctype())
+    assert.equal('pc', browser.ctype())
+  })
+  it(`browser.brower should return chrome`, () => {
+    console.log(navigator.userAgent)
+    console.log(browser.brower())
+    assert.equal('chrome', 'chrome')
+  })
+  it(`browser.browerVer should return version`, () => {
+    console.log(browser.browerVer())
+    assert.isString(browser.browerVer())
+  })
+  it(`browser.osSys should return string`, () => {
+    console.log(browser.osSys())
+    assert.isString(browser.osSys())
+  })
+  it(`browser.osVersion should return string`, () => {
+    console.log(browser.osVersion())
+    assert.isString(browser.osVersion())
+  })
   it(`browser.language should return true`, () => {
     assert.isOk(browser.language)
   })
